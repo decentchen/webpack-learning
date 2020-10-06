446337e15100920ab615a2b2ff82e5b8f0ec0e3b
JerryChen275 <870070493@qq.com>
2020-10-06 16:54:34 +0800

    modify

--- 4.webpack优化环境配置/23.code split/demo1/webpack.config.js
+++ 4.webpack优化环境配置/23.code split/demo1/webpack.config.js
@@ -1,27 +1,27 @@
-const { resolve } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // 单入口
   // entry: './src/js/index.js',
   entry: {
-    // 多入口：有一个入口，最终输出就有一个bundle
-    index: './src/js/index.js',
-    test: './src/js/test.js'
+    // 多入口：有几个入口，最终输出就有几个bundle（文件）
+    index: "./src/js/index.js",
+    test: "./src/js/test.js",
   },
   output: {
     // [name]：取文件名
-    filename: 'js/[name].[contenthash:10].js',
-    path: resolve(__dirname, 'build')
+    filename: "js/[name].[contenthash:10].js",
+    path: resolve(__dirname, "build"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
       minify: {
         collapseWhitespace: true,
-        removeComments: true
-      }
-    })
+        removeComments: true,
+      },
+    }),
   ],
-  mode: 'production'
+  mode: "production",
 };
